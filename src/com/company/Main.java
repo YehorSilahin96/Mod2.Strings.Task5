4ae5aacd1713decc599b02ce91d04c267d904884
YSilahin <egor.silagin96@gmail.com>
2021-06-16 15:28:35 +0300

    commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -15,28 +15,31 @@ public class Main {
         System.out.print("Enter email: ");
         String mail = in.nextLine();
 
+        //name check
         boolean res1 = name.matches("^([A-Z][a-z]*((\\s)))+[A-Z][a-z]*$");
         if (res1) {
             System.out.println("Name validation successful");
-        }
+            }
             else {
             System.out.println("Illegal. Name must contains only A-Z, a-z and space");
             }
 
+        //phone number check
         boolean res2 = num.matches("(\\+)\\d{12}");
         if (res2) {
             System.out.println("Phone number validation successful");
             }
             else {
-                System.out.println("Illegal. Phone number must contains only numbers and +");
+            System.out.println("Illegal. Phone number must contains + and 12 numbers");
             }
 
+        //email check
         boolean res3 = mail.matches("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$");
         if (res3) {
             System.out.println("Email validation successful");
-        }
+            }
             else {
-            System.out.println("Illegal. Email address must be in a format: (A-Z,0-9,_.+-)@(A-Z0-9.-).[A-Z]");
+            System.out.println("Illegal. Email address must be in a format: (A-Z,0-9,_.+-)@(A-Z,0-9.-).[A-Z]");
             }
         }
 
