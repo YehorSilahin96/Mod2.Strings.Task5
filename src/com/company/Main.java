3e25bf706093ac5e43e95876a72d19b82e7a9fd2
AnnaDremluga <dreanna3@gmail.com>
2021-06-16 23:10:04 +0300

    PR review

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,7 +7,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Scanner in = new Scanner(System.in);
+        Scanner in = new Scanner(System.in); // according to best practices which make code neat and clean, new variable must be created as much close to the code using it as it's possible
         System.out.print("Enter name and last name: ");
         String name = in.nextLine();
         System.out.print("Enter phone number: ");
@@ -15,15 +15,15 @@ public class Main {
         System.out.print("Enter email: ");
         String mail = in.nextLine();
 
-        boolean res1 = name.matches("^([A-Z][a-z]*((\\s)))+[A-Z][a-z]*$");
+        boolean res1 = name.matches("^([A-Z][a-z]*((\\s)))+[A-Z][a-z]*$"); // Always put strings like this to constants. Do you know why? 
         if (res1) {
             System.out.println("Name validation successful");
         }
-            else {
+            else { // please, fix formatting. 'else' must be on the same line with '}' 
             System.out.println("Illegal. Name must contains only A-Z, a-z and space");
-            }
+            } // please, fix indentation
 
-        boolean res2 = num.matches("(\\+)\\d{12}");
+        boolean res2 = num.matches("(\\+)\\d{12}"); // Always put this 'magic numbers' to constants
         if (res2) {
             System.out.println("Phone number validation successful");
             }
