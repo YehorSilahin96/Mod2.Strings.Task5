d73fab43a4072a40d8d1078183d67f116ee57bd3
YSilahin <egor.silagin96@gmail.com>
2021-06-18 18:25:33 +0300

    Added constant regex patterns

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,10 @@ import java.util.Scanner;
 
 
 public class Main {
+    final static String PATTERN_PHONE = "(\\+)\\d{12}";
+    final static String PATTERN_NAME = "^([A-Z][a-z]*((\\s)))+[A-Z][a-z]*$";
+    final static String PATTERN_EMAIL = "(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])";
+
 
     public static void main(String[] args) {
 
@@ -11,11 +15,11 @@ public class Main {
         System.out.print("Enter name and last name: ");
         String name = in.nextLine();
         System.out.print("Enter phone number: ");
-        String num = in.nextLine();
+        String number = in.nextLine();
         System.out.print("Enter email: ");
         String mail = in.nextLine();
 
-        boolean res1 = name.matches("^([A-Z][a-z]*((\\s)))+[A-Z][a-z]*$");
+        boolean res1 = name.matches(PATTERN_NAME);
         if (res1) {
             System.out.println("Name validation successful");
             }
@@ -23,15 +27,15 @@ public class Main {
             System.out.println("Illegal. Name must contains only A-Z, a-z and space");
             }
 
-        boolean res2 = num.matches("(\\+)\\d{12}");
+        boolean res2 = number.matches(PATTERN_PHONE);
         if (res2) {
             System.out.println("Phone number validation successful");
-            }
+        }
             else {
                 System.out.println("Illegal. Phone number must contains only numbers and +");
-            }
 
-        boolean res3 = mail.matches("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$");
+        }
+        boolean res3 = mail.matches(PATTERN_EMAIL);
         if (res3) {
             System.out.println("Email validation successful");
             }
